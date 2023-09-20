a5b5a1f2092806c741c05bfddbce78d5bfd58f68
cemgthedev <cemg.the.dev@gmail.com>
2023-09-20 15:54:58 -0300

    feat: Implementação da Tela de Edição de Perfil

---
+++ app/src/main/java/br/com/tiagors09/eletroshop/EdicaoDePerfil.java
@@ -0,0 +1,14 @@
+package br.com.tiagors09.eletroshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EdicaoDePerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edicao_de_perfil);
+    }
+}
