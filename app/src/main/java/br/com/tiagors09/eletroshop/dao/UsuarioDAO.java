6547cf6acb30b420649346b49fd9b0a34e405c6c
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-09-21 20:42:58 -0300

    feat: Adicionar métodos abstratos da interface

--- app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAO.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAO.java
@@ -1,5 +1,11 @@
 package br.com.tiagors09.eletroshop.dao;
 
-public interface UsuarioDAO {
+import java.util.UUID;
 
+import br.com.tiagors09.eletroshop.modelos.Usuario;
+
+public interface UsuarioDAO {
+    public boolean salvarUsuario(Usuario u);
+    public Usuario lerUsuario(UUID ID);
+    public boolean deletarUsuario(UUID ID);
 }
