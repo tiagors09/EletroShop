a3107117bfb7dbf1926fe226ea6e917ec3b4559f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-09-21 20:43:28 -0300

    feat: Implementar interface UsuarioDAO e sobreescrever seus métodos

--- app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAOImpl.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAOImpl.java
@@ -1,4 +1,61 @@
 package br.com.tiagors09.eletroshop.dao;
 
-public class UsuarioDAOImpl {
+import java.util.ArrayList;
+import java.util.UUID;
+
+import br.com.tiagors09.eletroshop.modelos.Usuario;
+
+public class UsuarioDAOImpl implements UsuarioDAO{
+    private ArrayList<Usuario> usuarios;
+    private static UsuarioDAO instance;
+
+    private UsuarioDAOImpl() {
+        usuarios = new ArrayList<Usuario>();
+    }
+
+    public static UsuarioDAO getInstance() {
+        if (instance == null) {
+            instance = new UsuarioDAOImpl();
+            return instance;
+        }
+
+        return instance;
+    }
+
+    @Override
+    public boolean salvarUsuario(Usuario u) {
+        int IDConvertido = Integer.parseInt(u.getID().toString());
+        boolean estaNoBanco = usuarios.contains(u);
+
+        if (estaNoBanco) {
+            usuarios.add(IDConvertido, u);
+            return true;
+        } else {
+            return usuarios.add(u);
+        }
+    }
+
+    @Override
+    public Usuario lerUsuario(UUID ID) {
+        int IDConvertido = Integer.parseInt(ID.toString());
+        Usuario usuario = usuarios.get(IDConvertido);
+
+        if (usuario != null) {
+            return usuario;
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean deletarUsuario(UUID ID) {
+        int IDConvertido = Integer.parseInt(ID.toString());
+        boolean estaNoBanco = usuarios.get(IDConvertido) != null;
+        if (estaNoBanco) {
+            usuarios.remove(IDConvertido);
+            return  true;
+        }
+
+        return false;
+    }
 }
