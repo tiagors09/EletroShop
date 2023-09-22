7d31348647447c8e319cb1bfeca4c6414a105225
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-09-21 21:11:39 -0300

    feat: Adicionar teste para instanciação do UsuarioDAO e adição de usuario no hashmap

--- app/src/test/java/br/com/tiagors09/eletroshop/UsuarioDAOTestCase.java
+++ app/src/test/java/br/com/tiagors09/eletroshop/UsuarioDAOTestCase.java
@@ -1,4 +1,33 @@
 package br.com.tiagors09.eletroshop;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.tiagors09.eletroshop.dao.UsuarioDAO;
+import br.com.tiagors09.eletroshop.dao.UsuarioDAOImpl;
+import br.com.tiagors09.eletroshop.modelos.Localizacao;
+import br.com.tiagors09.eletroshop.modelos.Usuario;
+
 public class UsuarioDAOTestCase {
+    private UsuarioDAO usuarioDAO = UsuarioDAOImpl.getInstance();
+    @Test
+    public void instanciarUsuarioDAO() {
+        assertNotNull(this.usuarioDAO);
+    }
+
+    @Test
+    public void adicionarUsuario() {
+        Usuario usuario = new Usuario(
+                "Tiago Rodrigues Sousa",
+                "28/01/2001",
+                new Localizacao(3.12, 4.12),
+                "Teste teste teste",
+                "tiagors09@gmail.com",
+                "Teste1234!");
+
+        assertNotNull(this.usuarioDAO);
+
+        assertTrue(usuarioDAO.salvarUsuario(usuario));
+    }
 }
