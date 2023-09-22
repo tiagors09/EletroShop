09f59a5cc1195f249d6b062dbf4f0af77c98af7a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-09-21 21:09:30 -0300

    feat: Mudar usuarios para HashMap
    Mudado o tipo do atributo `usuario` onde é guardado os usuarios da aplicação

--- app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAOImpl.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAOImpl.java
@@ -1,16 +1,16 @@
 package br.com.tiagors09.eletroshop.dao;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 import br.com.tiagors09.eletroshop.modelos.Usuario;
 
 public class UsuarioDAOImpl implements UsuarioDAO{
-    private ArrayList<Usuario> usuarios;
+    private HashMap<UUID,Usuario> usuarios;
     private static UsuarioDAO instance;
 
     private UsuarioDAOImpl() {
-        usuarios = new ArrayList<Usuario>();
+        usuarios = new HashMap<UUID,Usuario>();
     }
 
     public static UsuarioDAO getInstance() {
@@ -24,36 +24,22 @@ public class UsuarioDAOImpl implements UsuarioDAO{
 
     @Override
     public boolean salvarUsuario(Usuario u) {
-        int IDConvertido = Integer.parseInt(u.getID().toString());
-        boolean estaNoBanco = usuarios.contains(u);
-
-        if (estaNoBanco) {
-            usuarios.add(IDConvertido, u);
-            return true;
-        } else {
-            return usuarios.add(u);
-        }
+        usuarios.put(u.getID(), u);
+        return true;
     }
 
     @Override
     public Usuario lerUsuario(UUID ID) {
-        int IDConvertido = Integer.parseInt(ID.toString());
-        Usuario usuario = usuarios.get(IDConvertido);
-
-        if (usuario != null) {
-            return usuario;
-        }
-
-        return null;
+        return usuarios.get(ID);
     }
 
     @Override
     public boolean deletarUsuario(UUID ID) {
-        int IDConvertido = Integer.parseInt(ID.toString());
-        boolean estaNoBanco = usuarios.get(IDConvertido) != null;
-        if (estaNoBanco) {
-            usuarios.remove(IDConvertido);
-            return  true;
+        Usuario u = usuarios.get(ID);
+
+        if (u != null) {
+            usuarios.remove(ID);
+            return true;
         }
 
         return false;
