10cbe7e66914050e72f33859b7ecc7da67970bab
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-09-26 08:33:58 -0300

    refactor: Modificado assinatura de métodos existentes e adicionado novos métodos

--- app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAO.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAO.java
@@ -1,11 +1,14 @@
 package br.com.tiagors09.eletroshop.dao;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 import br.com.tiagors09.eletroshop.modelos.Usuario;
 
 public interface UsuarioDAO {
-    public boolean salvarUsuario(Usuario u);
-    public Usuario lerUsuario(UUID ID);
-    public boolean deletarUsuario(UUID ID);
+    public void salvar(Usuario u);
+    public void apagar(UUID ID);
+    public void atualizar(Usuario u);
+    public Usuario ler(UUID ID);
+    public HashMap<UUID, Usuario> lerTodosUsuarios();
 }
