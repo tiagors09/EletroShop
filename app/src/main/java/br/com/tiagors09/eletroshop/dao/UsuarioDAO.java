c71b0351dbdf074ffe3674d784216b32cb31fb37
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-09-26 08:54:27 -0300

    refactor: Remover atualizar e ajustar parâmetros dos métodos para usar CPF

--- app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAO.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAO.java
@@ -6,9 +6,8 @@ import java.util.UUID;
 import br.com.tiagors09.eletroshop.modelos.Usuario;
 
 public interface UsuarioDAO {
-    public void salvar(Usuario u);
-    public void apagar(UUID ID);
-    public void atualizar(Usuario u);
-    public Usuario ler(UUID ID);
-    public HashMap<UUID, Usuario> lerTodosUsuarios();
+    public Usuario salvar(Usuario u);
+    public Usuario apagar(String cpf);
+    public Usuario ler(String cpf);
+    public HashMap<String, Usuario> lerTodosUsuarios();
 }
