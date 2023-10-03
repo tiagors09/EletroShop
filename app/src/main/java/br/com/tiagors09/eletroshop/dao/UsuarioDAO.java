1a2ab9426c763abfe0654fc3d89f09318aa18f2f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-10-03 12:58:05 -0300

    refactor: Alterar tipo retorno lerTodos para Collection

--- app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAO.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/dao/UsuarioDAO.java
@@ -1,13 +1,12 @@
 package br.com.tiagors09.eletroshop.dao;
 
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.Collection;
 
 import br.com.tiagors09.eletroshop.modelos.Usuario;
 
 public interface UsuarioDAO {
-    public Usuario salvar(Usuario u);
-    public Usuario apagar(String cpf);
-    public Usuario ler(String cpf);
-    public HashMap<String, Usuario> lerTodosUsuarios();
+    Usuario salvar(Usuario u);
+    Usuario apagar(String cpf);
+    Usuario ler(String cpf);
+    Collection<Usuario> lerTodosUsuarios();
 }
