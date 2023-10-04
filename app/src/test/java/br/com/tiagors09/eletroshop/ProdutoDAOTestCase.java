78e00e89cb5e380d287a88de77049cca3288cb2b
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-10-04 12:31:05 -0300

    refactor: Melhorado testes

--- app/src/test/java/br/com/tiagors09/eletroshop/ProdutoDAOTestCase.java
+++ app/src/test/java/br/com/tiagors09/eletroshop/ProdutoDAOTestCase.java
@@ -13,26 +13,29 @@ import br.com.tiagors09.eletroshop.modelos.Usuario;
 public class ProdutoDAOTestCase {
     @Test
     public void inserirProduto() {
-        Usuario usuario = new Usuario(
+        Usuario detentor = new Usuario(
                 "01234567891",
                 "Tiago Rodrigues Sousa",
                 "28/01/2001",
-                new Localizacao(3.12, 4.12),
-                "Teste teste teste",
-                "tiagors09@gmail.com",
-                "Teste1234!");
-
-        Produto produto = new Produto(
-                "Geladeira branca",
-                340.00,
-                usuario,
-                "geladeira boa"
+                new Localizacao(3.1000, 4.000),
+                "Vendedor de produtos usados de Quixadá",
+                "tiagorodriguessousa9@gmail.com",
+                "SenhaTeste123"
         );
 
-        assertNotNull(usuario);
+        Produto produto = new Produto(
+                "Geladeira frost free",
+                300.00,
+                detentor,
+                "Geladeira novinha, sem problema, branca, 2015"
+        );
+
+        assertNotNull(detentor);
         assertNotNull(produto);
 
         ProdutoDAO produtoDAO = ProdutoDAOImpl.getInstance();
-        assertNotNull(produtoDAO.salvar(produto));
+        produtoDAO.salvar(produto);
+        assertNotNull(produtoDAO.ler(produto.getID()));
+        assertNotNull(produtoDAO.apagar(produto.getID()));
     }
 }
