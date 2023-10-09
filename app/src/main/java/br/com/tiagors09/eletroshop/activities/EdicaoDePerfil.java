74c4b6569cd0ff60ec75274a23cfaf3c8168da22
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-10-09 15:42:21 -0300

    refactor: Mover para activities

--- app/src/main/java/br/com/tiagors09/eletroshop/activities/EdicaoDePerfil.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/activities/EdicaoDePerfil.java
@@ -1,9 +1,11 @@
-package br.com.tiagors09.eletroshop;
+package br.com.tiagors09.eletroshop.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import br.com.tiagors09.eletroshop.R;
 
 public class EdicaoDePerfil extends AppCompatActivity {
 
