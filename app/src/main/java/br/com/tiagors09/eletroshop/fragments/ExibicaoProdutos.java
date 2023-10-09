3249fb71cd4b4358118ec8e559e34e7bb2280df5
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-10-09 15:46:03 -0300

    feat: Configurar exibição de produtos

--- app/src/main/java/br/com/tiagors09/eletroshop/fragments/ExibicaoProdutos.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/fragments/ExibicaoProdutos.java
@@ -1,6 +1,9 @@
 package br.com.tiagors09.eletroshop.fragments;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,8 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import br.com.tiagors09.eletroshop.R;
+import br.com.tiagors09.eletroshop.adapters.ProdutoAdapter;
+import br.com.tiagors09.eletroshop.dao.ProdutoDAO;
 
 public class ExibicaoProdutos extends Fragment {
+    private RecyclerView recyclerViewProdutos;
+    private ProdutoDAO produtoDAO;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,16 @@ public class ExibicaoProdutos extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exibicao_produtos, container, false);
+        View view = inflater.inflate(R.layout.fragment_exibicao_produtos, container, false);
+
+        recyclerViewProdutos = view.findViewById(R.id.recyclerViewProdutos);
+
+        ProdutoAdapter produtoAdapter = new ProdutoAdapter(produtoDAO.lerTodos());
+        recyclerViewProdutos.setAdapter(produtoAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(view.getContext(), 2);
+        recyclerViewProdutos.setLayoutManager(layoutManager);
+
+        return view;
     }
 }
