c3fd06256ad6b70ee425ef7bc4ecbb34ddc05f60
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-10-09 13:16:47 -0300

    feat: Definir comportamento da nav bar

--- app/src/main/java/br/com/tiagors09/eletroshop/MainActivity.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/MainActivity.java
@@ -1,16 +1,47 @@
 package br.com.tiagors09.eletroshop;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import br.com.tiagors09.eletroshop.fragments.ExibicaoProdutos;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottomNav);
+
+        loadFragment(new ExibicaoProdutos());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        if (item.getItemId() == R.id.home) {
+            loadFragment(new ExibicaoProdutos());
+        }
+
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.relativeLayout, fragment)
+                .commit();
     }
 }
