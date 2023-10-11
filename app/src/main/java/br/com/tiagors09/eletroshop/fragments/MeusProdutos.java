4cda4cadc11d7a2095860c004d58b312fdbfdd34
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-10-11 16:29:34 -0300

    feat: Criar fragment Meus Produtos

---
+++ app/src/main/java/br/com/tiagors09/eletroshop/fragments/MeusProdutos.java
@@ -0,0 +1,25 @@
+package br.com.tiagors09.eletroshop.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import br.com.tiagors09.eletroshop.R;
+
+public class MeusProdutos extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_meus_produtos, container, false);
+    }
+}
