8916a9f41c9914dc2a1064d3c730f62834aae049
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-10-11 16:29:16 -0300

    feat: Configurar navegação

--- app/src/main/java/br/com/tiagors09/eletroshop/activities/MainActivity.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/activities/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.tiagors09.eletroshop.activities;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,11 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import br.com.tiagors09.eletroshop.R;
+import br.com.tiagors09.eletroshop.fragments.Carrinho;
+import br.com.tiagors09.eletroshop.fragments.ExibicaoPerfil;
 import br.com.tiagors09.eletroshop.fragments.ExibicaoProdutos;
+import br.com.tiagors09.eletroshop.fragments.Favoritos;
+import br.com.tiagors09.eletroshop.fragments.MeusProdutos;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
     private BottomNavigationView bottomNav;
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         setContentView(R.layout.activity_main);
 
         bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(this);
 
         loadFragment(new ExibicaoProdutos());
     }
@@ -31,8 +37,23 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Fragment fragment = null;
 
-        if (item.getItemId() == R.id.home) {
+        if (item.getItemId() == R.id.home)
             loadFragment(new ExibicaoProdutos());
+
+        if (item.getItemId() == R.id.meus_produtos) {
+            loadFragment(new MeusProdutos());
+        }
+
+        if (item.getItemId() == R.id.carrinho) {
+            loadFragment(new Carrinho());
+        }
+
+        if (item.getItemId() == R.id.favoritos) {
+            loadFragment(new Favoritos());
+        }
+
+        if (item.getItemId() == R.id.perfil) {
+            loadFragment(new ExibicaoPerfil());
         }
 
         return true;
