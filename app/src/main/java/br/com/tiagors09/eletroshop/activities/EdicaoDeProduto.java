50aa0eb51df7d1146ff24de158b4bdd0351e5343
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-10-23 15:47:27 -0300

    refactor: Movido para activities

---
+++ app/src/main/java/br/com/tiagors09/eletroshop/activities/EdicaoDeProduto.java
@@ -0,0 +1,15 @@
+package br.com.tiagors09.eletroshop.activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.tiagors09.eletroshop.R;
+
+public class EdicaoDeProduto extends AppCompatActivity {
+}
