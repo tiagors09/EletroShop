10f64e0a36a7737a8641cf9fc14f9d3a6ffa0c8f
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-10-23 14:57:37 -0300

    feat: Definir comportamentos para edição de perfil e logout

--- app/src/main/java/br/com/tiagors09/eletroshop/fragments/ExibicaoPerfil.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/fragments/ExibicaoPerfil.java
@@ -2,17 +2,22 @@ package br.com.tiagors09.eletroshop.fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import br.com.tiagors09.eletroshop.R;
+import br.com.tiagors09.eletroshop.activities.EdicaoDePerfil;
+import br.com.tiagors09.eletroshop.activities.Login;
 
 public class ExibicaoPerfil extends Fragment {
+    private Button buttonLogout, buttonEditarPerfil;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,27 @@ public class ExibicaoPerfil extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_exibicao_perfil,container,false);
+        View view = inflater.inflate(R.layout.fragment_exibicao_perfil,container,false);
+
+        buttonLogout = view.findViewById(R.id.buttonLogout);
+        buttonEditarPerfil = view.findViewById(R.id.buttonEditarPerfil);
+
+        buttonEditarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), EdicaoDePerfil.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Login.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
