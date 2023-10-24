0e15ee4508e59a6c26f859263e4fa5252f201407
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-10-24 10:55:10 -0300

    feat: Adicionar holder individual

--- app/src/main/java/br/com/tiagors09/eletroshop/adapters/ProdutoEmOfertaAdapter.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/adapters/ProdutoEmOfertaAdapter.java
@@ -1,8 +1,10 @@
 package br.com.tiagors09.eletroshop.adapters;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,11 +12,13 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.UUID;
 
 import br.com.tiagors09.eletroshop.R;
+import br.com.tiagors09.eletroshop.activities.DetalheProdutoActivity;
 import br.com.tiagors09.eletroshop.modelos.Produto;
 
-public class ProdutoEmOfertaAdapter extends RecyclerView.Adapter<ProdutoHolder>{
+public class ProdutoEmOfertaAdapter extends RecyclerView.Adapter<ProdutoEmOfertaAdapter.ProdutoHolder>{
     private List<Produto> produtos;
 
     public ProdutoEmOfertaAdapter(List<Produto> produtos) {
@@ -23,12 +27,12 @@ public class ProdutoEmOfertaAdapter extends RecyclerView.Adapter<ProdutoHolder>{
 
     @NonNull
     @Override
-    public ProdutoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ProdutoEmOfertaAdapter.ProdutoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater
                 .from(parent.getContext())
                 .inflate(R.layout.produto_em_oferta, parent, false);
 
-        return new ProdutoHolder(view);
+        return new ProdutoEmOfertaAdapter.ProdutoHolder(view);
     }
 
     @Override
@@ -43,5 +47,19 @@ public class ProdutoEmOfertaAdapter extends RecyclerView.Adapter<ProdutoHolder>{
     @Override
     public int getItemCount() {
         return produtos.size();
+    }
+
+    class ProdutoHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewTitulo, textViewPreco;
+        public ImageView imageViewFoto;
+
+        public ProdutoHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.textViewTitulo = itemView.findViewById(R.id.textViewTitulo);
+            this.textViewPreco = itemView.findViewById(R.id.textViewPreco);
+            this.imageViewFoto = itemView.findViewById(R.id.imageViewFoto);
+        }
     }
 }
