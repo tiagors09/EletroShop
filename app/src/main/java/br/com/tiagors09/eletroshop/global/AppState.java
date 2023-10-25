a2a70dfb598cfdd36e3947d36c26e0bcc2c9b65f
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-10-25 14:42:23 -0300

    feat: Criar classe AppState para guardar chaves globais do app

---
+++ app/src/main/java/br/com/tiagors09/eletroshop/global/AppState.java
@@ -0,0 +1,8 @@
+package br.com.tiagors09.eletroshop.global;
+
+import br.com.tiagors09.eletroshop.modelos.Usuario;
+
+public class AppState {
+    public static boolean estaLogado = false;
+    public static Usuario usuario = null;
+}
