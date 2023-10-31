5cdbda372853772735931326cda3f1a839cd554d
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-10-31 15:59:04 -0300

    feat: Configurado Layout Manager e Adapter

--- app/src/main/java/br/com/tiagors09/eletroshop/fragments/HistoricoFragment.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/fragments/HistoricoFragment.java
@@ -3,6 +3,7 @@ package br.com.tiagors09.eletroshop.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -10,13 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import br.com.tiagors09.eletroshop.R;
+import br.com.tiagors09.eletroshop.adapters.TransacaoAdapter;
+import br.com.tiagors09.eletroshop.dao.TransacaoDAO;
+import br.com.tiagors09.eletroshop.dao.TransacaoDAOImpl;
 
 public class HistoricoFragment extends Fragment {
     private RecyclerView recyclerViewHistorico;
+    private TransacaoDAO transacaoDAO;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        transacaoDAO = TransacaoDAOImpl.getInstance();
     }
 
     @Override
@@ -25,6 +31,14 @@ public class HistoricoFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_historico, container, false);
 
         recyclerViewHistorico = v.findViewById(R.id.recyclerViewHistorico);
+
+        recyclerViewHistorico.setLayoutManager(
+                new LinearLayoutManager(v.getContext())
+        );
+
+        recyclerViewHistorico.setAdapter(
+                new TransacaoAdapter(transacaoDAO.lerTodasTransacoes())
+        );
 
         return v;
     }
