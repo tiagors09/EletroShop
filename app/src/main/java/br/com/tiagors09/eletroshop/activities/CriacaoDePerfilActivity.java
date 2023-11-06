9511a1b8c335a0ed2571b4b32998809e291ecfdb
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-11-06 16:04:07 -0300

    feat: Adicionar lógica para criação de perfil e persistência no Firebase

--- app/src/main/java/br/com/tiagors09/eletroshop/activities/CriacaoDePerfilActivity.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/activities/CriacaoDePerfilActivity.java
@@ -6,16 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import br.com.tiagors09.eletroshop.R;
+import br.com.tiagors09.eletroshop.modelos.Localizacao;
+import br.com.tiagors09.eletroshop.modelos.Usuario;
 
 public class CriacaoDePerfilActivity extends AppCompatActivity {
     private Button btnCancelar, btnSalvar;
+    private TextInputEditText cpf, nome, dataNasc, localizacao, email, senha, bio;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criacao_de_perfil);
+
+        cpf = findViewById(R.id.cpf);
+        nome = findViewById(R.id.nome);
+        dataNasc = findViewById(R.id.dataNasc);
+        localizacao = findViewById(R.id.localizacao);
+        email = findViewById(R.id.email);
+        senha = findViewById(R.id.senha);
+        bio = findViewById(R.id.bio);
 
         btnSalvar = findViewById(R.id.buttonSalvar);
         btnCancelar = findViewById(R.id.buttonCancelar);
@@ -23,19 +44,41 @@ public class CriacaoDePerfilActivity extends AppCompatActivity {
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(
-                        new Intent(getApplicationContext(),MainActivity.class)
-                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                );
+                firebaseAuth
+                        .createUserWithEmailAndPassword(
+                                email.getText().toString(),
+                                senha.getText().toString()
+                        )
+                        .addOnSuccessListener(CriacaoDePerfilActivity.this, new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                databaseReference.child("usuarios").push().setValue(
+                                        new Usuario(
+                                                cpf.getText().toString(),
+                                                nome.getText().toString(),
+                                                dataNasc.getText().toString(),
+                                                new Localizacao(2, 3),
+                                                bio.getText().toString(),
+                                                email.getText().toString()
+                                        )
+                                )
+                                        .addOnSuccessListener(CriacaoDePerfilActivity.this, new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Toast
+                                                        .makeText(CriacaoDePerfilActivity.this, "Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        });
+                            }
+                        });
             }
         });
 
         btnCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), LoginActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
+                finish();
             }
         });
     }
