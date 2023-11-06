eb586b5d8505b26cc9b94482252c000bac1594c3
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-11-06 16:04:18 -0300

    feat: Adicionar lógica para login

--- app/src/main/java/br/com/tiagors09/eletroshop/activities/LoginActivity.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package br.com.tiagors09.eletroshop.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.tiagors09.eletroshop.R;
 import br.com.tiagors09.eletroshop.dao.UsuarioDAO;
@@ -20,7 +28,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText email, senha;
     private TextView textViewCadastro;
     private Button buttonLogin;
-    private UsuarioDAO usuarioDAO;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,27 +40,38 @@ public class LoginActivity extends AppCompatActivity {
         textViewCadastro = findViewById(R.id.textViewCadastro);
         buttonLogin = findViewById(R.id.buttonSalvar);
 
-        usuarioDAO = UsuarioDAOImpl.getInstance();
-
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                String emailUsuario = email.getText().toString();
-//                String senhaUsuario = senha.getText().toString();
-//
-//                Usuario usuario = usuarioDAO.ler(emailUsuario);
-//                if (usuario.getPassword().equals(senhaUsuario)) {
-//                    AppState.estaLogado = true;
-//                    AppState.usuario = usuario;
-//                    startActivity(
-//                            new Intent(getApplicationContext(),MainActivity.class)
-//                                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
-//                    );
-//                }
-                startActivity(
-                        new Intent(getApplicationContext(),MainActivity.class)
-                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                );
+                String emailUsuario = email.getText().toString();
+                String senhaUsuario = senha.getText().toString();
+
+                firebaseAuth
+                        .signInWithEmailAndPassword(
+                            emailUsuario,
+                            senhaUsuario
+                        )
+                        .addOnSuccessListener(LoginActivity.this, new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(
+                                        new Intent(getApplicationContext(),MainActivity.class)
+                                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                                );
+                            }
+                        })
+                        .addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast
+                                        .makeText(
+                                                LoginActivity.this,
+                                                "Dados de Login inválidos",
+                                                Toast.LENGTH_SHORT
+                                        )
+                                        .show();
+                            }
+                        });
             }
         });
 
@@ -60,7 +80,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(
                         new Intent(getApplicationContext(), CriacaoDePerfilActivity.class)
-                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 );
             }
         });
