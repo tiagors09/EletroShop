864f8f587688d74f883dc6f095a2f9a03c2f0cfd
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-11-07 12:35:12 -0300

    refactor: Mudar forma de mudar fragments

--- app/src/main/java/br/com/tiagors09/eletroshop/activities/MainActivity.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.tiagors09.eletroshop.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -9,6 +10,11 @@ import androidx.core.splashscreen.SplashScreen;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import br.com.tiagors09.eletroshop.R;
 import br.com.tiagors09.eletroshop.fragments.CarrinhoFragment;
@@ -17,7 +23,7 @@ import br.com.tiagors09.eletroshop.fragments.ExibicaoProdutosFragment;
 import br.com.tiagors09.eletroshop.fragments.HistoricoFragment;
 import br.com.tiagors09.eletroshop.fragments.MeusProdutosFragment;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNav;
 
     @Override
@@ -27,35 +33,27 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         setContentView(R.layout.activity_main);
 
         bottomNav = findViewById(R.id.bottomNav);
-        bottomNav.setOnNavigationItemSelectedListener(this);
+
+        Map<Integer, Fragment> fragments = new HashMap<Integer, Fragment>(){{
+                put(R.id.home, new ExibicaoProdutosFragment());
+                put(R.id.meus_produtos, new MeusProdutosFragment());
+                put(R.id.carrinho, new CarrinhoFragment());
+                put(R.id.favoritos, new HistoricoFragment());
+                put(R.id.perfil, new ExibicaoPerfilFragment());
+        }};
+
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                loadFragment(fragments.get(item.getItemId()));
+
+                return true;
+            }
+        });
 
         loadFragment(new ExibicaoProdutosFragment());
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment fragment = null;
-
-        if (item.getItemId() == R.id.home)
-            loadFragment(new ExibicaoProdutosFragment());
-
-        if (item.getItemId() == R.id.meus_produtos) {
-            loadFragment(new MeusProdutosFragment());
-        }
-
-        if (item.getItemId() == R.id.carrinho) {
-            loadFragment(new CarrinhoFragment());
-        }
-
-        if (item.getItemId() == R.id.favoritos) {
-            loadFragment(new HistoricoFragment());
-        }
-
-        if (item.getItemId() == R.id.perfil) {
-            loadFragment(new ExibicaoPerfilFragment());
-        }
-
-        return true;
     }
 
     private void loadFragment(Fragment fragment) {
