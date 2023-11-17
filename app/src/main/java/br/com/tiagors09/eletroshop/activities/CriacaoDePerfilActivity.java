48b4e03417cf54e69eee07abf3006ab91dcd65a7
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-11-17 15:06:15 -0300

    refactor: removido mock

--- app/src/main/java/br/com/tiagors09/eletroshop/activities/CriacaoDePerfilActivity.java
+++ app/src/main/java/br/com/tiagors09/eletroshop/activities/CriacaoDePerfilActivity.java
@@ -1,21 +1,14 @@
 package br.com.tiagors09.eletroshop.activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 
@@ -27,7 +20,7 @@ import br.com.tiagors09.eletroshop.modelos.Usuario;
 
 public class CriacaoDePerfilActivity extends AppCompatActivity {
     private Button btnCancelar, btnSalvar;
-    private TextInputEditText email, senha;
+    private TextInputEditText textInputEditTextNome, textInputEditTextDataNasc, textInputEditTextLocal, textInputEditTextEmail, textInputEditTextSenha, textInputEditTextBio;
     private TextInputEditText[] campos;
     private UsuarioDAO usuarioDAO;
 
@@ -36,10 +29,14 @@ public class CriacaoDePerfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criacao_de_perfil);
 
-        email = findViewById(R.id.email);
-        senha = findViewById(R.id.senha);
+        textInputEditTextNome = findViewById(R.id.textInputEditTextNome);
+        textInputEditTextDataNasc = findViewById(R.id.textInputEditTextDataNasc);
+        textInputEditTextLocal = findViewById(R.id.textInputEditTextLocal);
+        textInputEditTextEmail = findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextSenha = findViewById(R.id.textInputEditTextSenha);
+        textInputEditTextBio = findViewById(R.id.textInputEditTextBio);
 
-        campos = new TextInputEditText[]{email, senha};
+        campos = new TextInputEditText[]{textInputEditTextNome, textInputEditTextDataNasc, textInputEditTextLocal, textInputEditTextEmail, textInputEditTextSenha, textInputEditTextBio};
 
         btnSalvar = findViewById(R.id.buttonSalvar);
         btnCancelar = findViewById(R.id.buttonCancelar);
@@ -53,13 +50,11 @@ public class CriacaoDePerfilActivity extends AppCompatActivity {
                 usuarioDAO
                         .salvar(
                                 new Usuario(
-                                        "123.456.784.353.95",
-                                        "Tiago",
-                                        "28/01/2001",
-                                        new Localizacao(2, 3),
-                                        "teste",
-                                        email.getText().toString(),
-                                        senha.getText().toString()
+                                        textInputEditTextNome.getText().toString(),
+                                        textInputEditTextDataNasc.getText().toString(),
+                                        textInputEditTextBio.getText().toString(),
+                                        textInputEditTextEmail.getText().toString(),
+                                        textInputEditTextSenha.getText().toString()
                                 )
                         )
                         .addOnSuccessListener(
@@ -72,6 +67,9 @@ public class CriacaoDePerfilActivity extends AppCompatActivity {
                                                         Toast.LENGTH_SHORT
                                                 )
                                                 .show();
+
+
+                                        finish();
                                     }
                                 }
                         )
@@ -87,12 +85,7 @@ public class CriacaoDePerfilActivity extends AppCompatActivity {
             }
         });
 
-        btnCancelar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        btnCancelar.setOnClickListener(view -> finish());
     }
 
     private void validarCampos() {
